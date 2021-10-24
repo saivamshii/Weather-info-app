d06f327f9169e016bfba6e79257a0c0646bc7771
saivamshii <84490491+saivamshii@users.noreply.github.com>
2021-10-24 17:03:58 +0530

    Add project files.

---
+++ Weatherr/Weatherr/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Weatherr
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            //handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            //handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+}
---
+++ Weatherr/Weatherr/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Weatherr
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+        public List<Weather> Weathers { get => WeatherData(); }
+
+        private List<Weather> WeatherData()
+        {
+            var tempList = new List<Weather>();
+            tempList.Add(new Weather { Temp = "22",  Date = "Sunday 16", Icon = "weather.png" });
+            tempList.Add(new Weather { Temp = "21", Date = "Monday 17", Icon = "cloud.png" });
+            tempList.Add(new Weather { Temp = "20", Date = "Tuesday 18", Icon = "weather.png" });
+            tempList.Add(new Weather { Temp = "12", Date = "Wednesday 19", Icon = "humidity.png" });
+            tempList.Add(new Weather { Temp = "17", Date = "Thursday 20", Icon = "weather.png" });
+            tempList.Add(new Weather { Temp = "20", Date = "Friday 21", Icon = "wind.png" });
+
+            return tempList;
+        }
+         
+
+
+    }
+    public class Weather
+    {
+        public string Temp { get; set; }
+        public string Date { get; set; }
+         public string Icon { get; set; }
+    }
+}
